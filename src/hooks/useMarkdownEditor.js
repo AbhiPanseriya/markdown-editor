6c6e47ff9d393cb10377f680f3d6a670eaaf8c73
AbhiPanseriya <abhipanseriya@gmail.com>
2021-04-17 21:11:28 +0530

    feat: added localstorage to store the input

--- src/hooks/useMarkdownEditor.js
+++ src/hooks/useMarkdownEditor.js
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 const useMarkDownEditor = () => {
-    const [text, setText] = useState('');
+    const [text, setText] = useState(localStorage.getItem('text') || '');
     const handleChangeRawInputedText = (text) => {
         setText(text);
+        localStorage.setItem('text', text);
     }
     const getMarkDownAsHTMLOutput = () => {
         return <ReactMarkdown children={text} />
